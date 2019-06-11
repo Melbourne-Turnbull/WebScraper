7e608c8f6487eb723dd4f86497fe1e76e3af8f61
Melbourne Turnbull <TurnbullMT@cmog.org>
2019-06-11 13:43:52 -0400

    Added comments

--- HaveIBeenPwnedButBetter/Program.cs
+++ HaveIBeenPwnedButBetter/Program.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.IO;
 using Newtonsoft.Json;
 
-namespace HaveIBeenPwnedApi
+namespace HaveIBeenPwnedButBetter
 {
     public class Pwned
     {
@@ -25,7 +25,7 @@ namespace HaveIBeenPwnedApi
         public string LogoPath { get; set; }
 
         private static string API_RootPath = "https://haveibeenpwned.com/api/v2";
-        private static string API_BreachServicePath = API_RootPath+"/breachedaccount/";
+        private static string API_BreachServicePath = API_RootPath + "/breachedaccount/";
 
 
         //----------------------------------------------------------------------------------------------------------------
@@ -37,67 +37,15 @@ namespace HaveIBeenPwnedApi
                 $" IsFabricated: {IsFabricated} \n \t IsSensitive: {IsSensitive} \n \t IsRetired: {IsRetired} \n \t" +
                 $" IsSpamList: {IsSpamList} \n \t LogoPath: {LogoPath}";
         }
-
         //----------------------------------------------------------------------------------------------------------------
-        public static HttpWebRequest RequestWebpage(string email, HttpWebRequest webRequest)
+        public static IEnumerable<Pwned> APIEndpoint(string email)       //Make independant endpoint
         {
-            //I like this model, I would chase down the reason for the buggy response behavior
-            if (webRequest == null)
-            {
-                return null;
-            }
-            webRequest.Method = "GET";
-            webRequest.ContentType = "application/json";
-            webRequest.UserAgent = "HaveIBeenPwnedAPIExample";
-            return webRequest;
-        }
-        //----------------------------------------------------------------------------------------------------------------
-        public static List<Pwned> DeserializeToObject(string jsonString)
-        {
-            if (!String.IsNullOrEmpty(jsonString))
-                return JsonConvert.DeserializeObject<List<Pwned>>(jsonString);
-            else
-                return new List<Pwned>();  //empty list
-        }
-        //----------------------------------------------------------------------------------------------------------------
-        public static IEnumerable<Pwned> Argument(List<Pwned> result)
-        {
-            IEnumerable<Pwned> list = result.FindAll(x => x.IsSpamList.Equals(false));
-            foreach (Pwned IsSpamList in list)
-            {
-                Console.WriteLine(IsSpamList);
-            }
-            return list;
-        }
-        //----------------------------------------------------------------------------------------------------------------
-        public static void PrintToConsole(List<Pwned> result)
-        {
-            result.ForEach(Console.WriteLine);
-        }
-        //----------------------------------------------------------------------------------------------------------------
-        public static List<Pwned> CheckAllEmails(List<string> emails)
-        {
-            //confused about why i'm receiving an error message here...  trying a few things
-            List<Pwned> result = new List<Pwned>();
-            foreach (string email in emails)
-            {
-                IEnumerable<Pwned> breaches = CheckSpecificEmail(email);
-                result.AddRange(breaches);
-            }
-            return result;
-        }
-        //----------------------------------------------------------------------------------------------------------------
-        public static IEnumerable<Pwned> CheckSpecificEmail(string email)
-        {
-            //Console.WriteLine("Please enter email address to check");
-            //var email = Console.ReadLine();
             var requestPath = String.Format("{0}/{1}", Pwned.API_BreachServicePath, email);
             string ResultAsJson = string.Empty;
             List<Pwned> result = new List<Pwned>();
 
             HttpWebRequest webRequest = WebRequest.Create(requestPath) as HttpWebRequest;
 
-            //bringing this back into the current method for now, will leave deserialization as separate method
             if (webRequest == null)
             {
                 return null;
@@ -113,20 +61,48 @@ namespace HaveIBeenPwnedApi
                     using (var sr = new StreamReader(s))
                     {
                         ResultAsJson = sr.ReadToEnd();
+                        result.AddRange(DeserializeToObject(ResultAsJson));
+                        return result;
                     }
                 }
             }
             catch (System.Net.WebException)
             {
-                //nothing to do here, ResultAsJson remains empty
-                //return null;
             }
-
-            //RequestWebpage(email, webRequest); //my concern with this is that it doesn't do the whole web request, so the name is misleading
-            //DeserializeToObject(email, webRequest); //my concern with this is that this function *does* do part of the web request, so that element of the process is split among two functions
-
-            result.AddRange(DeserializeToObject(ResultAsJson));
-                       
+            return result;
+        }
+        //----------------------------------------------------------------------------------------------------------------
+        public static List<Pwned> DeserializeToObject(string ResultAsJson)        //Seems fine so far
+        {
+            if (!String.IsNullOrEmpty(ResultAsJson))
+                return JsonConvert.DeserializeObject<List<Pwned>>(ResultAsJson);
+            else
+                return new List<Pwned>();  //empty list
+        }
+        //----------------------------------------------------------------------------------------------------------------
+        /*public static IEnumerable<Pwned> Argument(List<Pwned> result)        //Not important at the moment
+        {
+            IEnumerable<Pwned> list = result.FindAll(x => x.IsSpamList.Equals(false));
+            foreach (Pwned IsSpamList in list)
+            {
+                Console.WriteLine(IsSpamList);
+            }
+            return list;
+        }*/
+        //----------------------------------------------------------------------------------------------------------------
+        public static void PrintToConsole(List<Pwned> result)
+        {
+            result.ForEach(Console.WriteLine);
+        }
+        //----------------------------------------------------------------------------------------------------------------
+        public static List<Pwned> CheckAllEmails(List<string> emails)      //Seems fine for the moment
+        {
+            List<Pwned> result = new List<Pwned>();
+            foreach (string email in emails)
+            {
+                IEnumerable<Pwned> breaches = APIEndpoint(email);
+                result.AddRange(breaches);
+            }
             return result;
         }
     }
@@ -135,15 +111,15 @@ namespace HaveIBeenPwnedApi
     {
         public static void Main(string[] args)
         {
-            bool debugging = true ;
+            bool debugging = true;
             //I want this here because this is the easiest place to populate the list from a commandline argument
             var emailsToCheck = new List<string>() { "mturnbull2@yahoo.com", "turnbullmt05@mansfield.edu", "turnbullmt@cmog.org" };
             //var emailsToCheck = new List<string>(args);
             List<Pwned> AllBreaches = CheckAllEmails(emailsToCheck);
             if (debugging)
                 PrintToConsole(AllBreaches);
-            else;  
-                //do something else, like write to SQL server
+            else;
+            //do something else, like write to SQL server
 
             Console.ReadKey();
         }
---
+++ HaveIBeenPwnedFromScratch/Program.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace HaveIBeenPwnedFromScratch
+{
+    public class Pwned
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Domain { get; set; }
+        public DateTime BreachDate { get; set; }
+        public DateTime AddedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int PwnCount { get; set; }
+        public string Description { get; set; }
+        public string[] DataClasses { get; set; }
+        public Boolean IsVerified { get; set; }
+        public Boolean IsFabricated { get; set; }
+        public Boolean IsSensitive { get; set; }
+        public Boolean IsRetired { get; set; }
+        public Boolean IsSpamList { get; set; }
+        public string LogoPath { get; set; }
+
+        //Path to the API
+        private static string API_RootPath = "https://haveibeenpwned.com/api/v2";
+        private static string API_BreachServicePath = API_RootPath + "/breachedaccount/";
+        //----------------------------------------------------------------------------------------------------------------
+        public override string ToString()
+        {
+            //format for the returned value
+            return $"Name: {Name} \n \t Title: {Title} \n \t Domain: {Domain} \n \t Breach Date: {BreachDate} \n \t" +
+                $" Added Date: {AddedDate} \n \t Modified Date: {ModifiedDate} \n \t PwnCount: {PwnCount} \n \t" +
+                $" Description: {Description} \n \t DataClasses: {DataClasses} \n \t IsVerified: {IsVerified} \n \t" +
+                $" IsFabricated: {IsFabricated} \n \t IsSensitive: {IsSensitive} \n \t IsRetired: {IsRetired} \n \t" +
+                $" IsSpamList: {IsSpamList} \n \t LogoPath: {LogoPath}";
+        }
+        //----------------------------------------------------------------------------------------------------------------
+        public static IEnumerable<Pwned> RESTendpoint(string email)
+        {
+            //finishes the path with the email thats being checked
+            var requestPath = String.Format("{0}/{1}", Pwned.API_BreachServicePath, email);
+            string ResultAsJson = string.Empty; //place holder
+            List<Pwned> result = new List<Pwned>();
+
+            //sending the URL with the request
+            HttpWebRequest webRequest = WebRequest.Create(requestPath) as HttpWebRequest;
+
+            if (webRequest == null)
+            {
+                return null;
+            }
+            webRequest.Method = "GET";
+            webRequest.ContentType = "application/json";
+            webRequest.UserAgent = "HaveIBeenPwnedAPIExample";
+
+            //gets response and reads it before sending it to get deserialized
+            try
+            {
+                using (Stream s = webRequest.GetResponse().GetResponseStream())
+                {
+                    using (var sr = new StreamReader(s))
+                    {
+                        ResultAsJson = sr.ReadToEnd();
+                        result.AddRange(Convert(ResultAsJson));
+                        return result;
+                    }
+                }
+            }
+            //if the URL is invalid/ there was no breaches it returns nothing
+            catch (System.Net.WebException)
+            {
+            }
+            return result;
+        }
+        //----------------------------------------------------------------------------------------------------------------
+        public static List<Pwned> SendRequest(List<string> emailList)
+        {
+            //brings in list of emails and sends to RESTendpoint to check for breaches
+            List<Pwned> result = new List<Pwned>();
+            foreach (string email in emailList)
+            {
+                result.AddRange(RESTendpoint(email));
+            }
+            //Returns the breaches deserialized
+            return result;
+        }
+        //----------------------------------------------------------------------------------------------------------------
+        public static List<Pwned> Convert(string ResultAsJson)
+        {
+            //convert from string to generic list (deserialize method)
+            if (!String.IsNullOrEmpty(ResultAsJson))
+            {
+                return JsonConvert.DeserializeObject<List<Pwned>>(ResultAsJson);
+            }
+            else
+            {
+                return new List<Pwned>();  //empty list
+            }
+        }
+        //----------------------------------------------------------------------------------------------------------------
+        public static void NewEndpoint(List<Pwned> result, bool SendToDatabase)
+        {
+            //Boolean in main to check if its being sent to database or console
+            if (SendToDatabase)
+            {
+                //send to MsSql database
+            }
+            else
+            {
+                //print to console 
+                result.ForEach(Console.WriteLine);
+            }
+        }
+        //----------------------------------------------------------------------------------------------------------------
+    }
+    class Program : Pwned
+    {
+        static void Main(string[] args)
+        {
+            bool SendToDatabase = false;
+            var emailList = new List<string>(args) { "mturnbull2@yahoo.com", "turnbullmt05@mansfield.edu", "turnbullmt@cmog.org" };
+            List<Pwned> AllBreaches = SendRequest(emailList);
+            NewEndpoint(AllBreaches, SendToDatabase);
+            Console.ReadKey();
+        }
+    }
+}
